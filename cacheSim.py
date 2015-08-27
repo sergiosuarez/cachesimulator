52383bef98c0256ac0c223c373dcd0338a4cb79c
Juan Mite <juanmisak@gmail>
2015-08-26 23:58:01 -0500

    LRU policy and plot has been implemented.
        For the plot I used matplotlib library.

--- cacheSim.py
+++ cacheSim.py
@@ -1,4 +1,33 @@
-#! /usr/bin/python
+#! /usr/bin/python2.7
+# encoding=utf8
 import sys
-workload = open(sys.argv[1], 'r')
-print (workload.read(10), sys.argv[2], sys.argv[3])
+
+workload_file = sys.argv[1]
+policy = sys.argv[2]
+cache_tam = int(sys.argv[3])
+cache = []
+misses = 0
+hites = 0
+
+if policy.upper() == 'LRU':
+    print 'Evaluando una caché LRU con '+ str(cache_tam) +' entradas...'
+    with open(workload_file) as f:
+        for line in f:
+            if (cache.__contains__(line)):
+                # HIT
+                cache.remove(line)
+                cache.append(line)
+                hites = hites +1
+                #print(line, 'hit', cache)
+            else:
+                # MISS
+                misses = misses + 1
+                cache.append(line)
+                if len(cache) > cache_tam :
+                    cache.remove(cache[0])
+                #print(line, 'miss', cache)
+    print('Resultados: ')
+    print ('\tMiss rate: ', '\t('+str(misses)+' misses out of '+' -- '+' references)')
+    print ('\tMiss rate (warm cache): ', '\t('+str(hites)+' misses out of '+'--cache_tam--' +' references)')
+
+    
